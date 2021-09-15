9164d8fc1fc2601c37acd3843f5332ee548945ac
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-09-15 13:37:13 +0200

    52-creating-recipe-model-add-recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Spaghetti Bolognese', 
+      'Elke Italiaanse nonna heeft haar eigen bologneserecept.', 
+      'https://static.ah.nl/static/recepten/img_RAM_PRD121467_890x594_JPG.jpg')
+  ];
 
   constructor() { }
 
