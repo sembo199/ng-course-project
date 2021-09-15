03b684c0bc1f3bea4e712ab590ea37648db20e37
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-09-15 13:53:31 +0200

    54-displaying-recipes-with-ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,7 +12,11 @@ export class RecipeListComponent implements OnInit {
     new Recipe(
       'Spaghetti Bolognese', 
       'Elke Italiaanse nonna heeft haar eigen bologneserecept.', 
-      'https://static.ah.nl/static/recepten/img_RAM_PRD121467_890x594_JPG.jpg')
+      'https://static.ah.nl/static/recepten/img_RAM_PRD121467_890x594_JPG.jpg'),
+    new Recipe(
+      'Pizza Hawaï', 
+      'Pizza met ananas? Ja! Deze is voor alle pizza Hawaï lovers.', 
+      'https://static.ah.nl/static/recepten/img_RAM_PRD134303_890x594_JPG.jpg')
   ];
 
   constructor() { }
