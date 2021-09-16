216268563d6ec08b2059911a8599306b0673255f
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-09-16 13:03:49 +0200

    89-adding-ingredients-to-shopping-list-with-event-and-prop-binding

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() addIngredient = new EventEmitter<Ingredient>();
+  @ViewChild('nameInput', {static: true}) nameInput: ElementRef;
+  @ViewChild('amountInput', {static: true}) amountInput: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    const ingredient: Ingredient = new Ingredient(
+      this.nameInput.nativeElement.value,
+      this.amountInput.nativeElement.value
+    );
+    this.addIngredient.emit(ingredient);
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -18,4 +18,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onAddIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+
 }
