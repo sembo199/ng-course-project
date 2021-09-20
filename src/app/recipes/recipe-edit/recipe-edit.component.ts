0878d68f23445904a2ca5f7731dd8eb7c8193568
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-09-20 11:50:30 +0200

    165-retrieving-route-parameters

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+  id: number;
+  recipe: Recipe;
+  editMode = false;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params.id;
+      this.editMode = params['id'] != null;
+      if (this.editMode) {
+        this.recipe = this.recipeService.getRecipe(this.id);
+      }
+    });
   }
 
 }
