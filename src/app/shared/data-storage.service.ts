14bbda6c61c11d74d0e47bd870714ec74900c396
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-09-21 16:15:23 +0200

    280-storing-recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,26 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core"
+import { Recipe } from "../recipes/recipe.model";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
+  private recipeUrl = 'https://ng-course-project-277f1-default-rtdb.europe-west1.firebasedatabase.app/recipes.json';
+
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private recipeService: RecipeService
   ) {}
+
+  storeRecipes() {
+    // Request
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(
+      this.recipeUrl,
+      recipes
+    )
+    .subscribe(response => {
+      console.log(response);
+    });
+  }
 }
