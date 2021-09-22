22e650f48ea5428d511da8deb0116a3681763a82
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-09-22 14:09:34 +0200

    287-adding-the-auth-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,11 +15,13 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { AuthComponent } from './auth/auth.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    AuthComponent,
     HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
