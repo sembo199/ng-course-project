68888d07225b351da91ff29f5c92df26f51e2abd
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-09-23 13:13:16 +0200

    322-adding-required-import-modules

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,5 +1,7 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { AppRoutingModule } from "../app-routing.module";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
@@ -15,6 +17,11 @@ import { RecipesComponent } from "./recipes.component";
     RecipeListComponent,
     RecipeItemComponent,
     RecipeStartComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule
   ],
   exports: [
     RecipesComponent,
