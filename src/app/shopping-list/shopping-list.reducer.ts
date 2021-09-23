10f1fa03d62b377538403457cc61fb7406cd881c
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-09-23 16:52:32 +0200

    346-getting-started-with-reducers

---
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -0,0 +1,13 @@
+import { Ingredient } from "../shared/ingredient.model";
+
+const initialState = {
+  ingredients: [
+    new Ingredient('Spaghetti', 1),
+    new Ingredient('Pasta saus', 1),
+    new Ingredient('Rundergehakt', 1)
+  ]
+};
+
+export function shoppingListReducer(state, action) {
+
+}
