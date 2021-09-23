589a2ac0c4ad309ef570788ce7ef5f2a843775b3
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-09-23 09:31:29 +0200

    301-attaching-token-with-interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -29,20 +29,17 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.authService.user.pipe(take(1), exhaustMap(user => { 
-      return this.http
-        .get<Recipe[]>(this.recipeUrl,
-        {
-          params: new HttpParams().set('auth', user.token)
-        });
-    }),
-    map(recipes => {
-      return recipes.map(recipe => {
-        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-      });
-    }),
-    tap(recipes => {
-      this.recipeService.setRecipes(recipes);
-    }));
+    return this.http
+      .get<Recipe[]>(this.recipeUrl)
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
   }
 }
