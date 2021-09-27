c8dea173d4cc78e9efb9ccba9ef2203279807da5
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-09-27 16:54:42 +0200

    425-testing-dependencies

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from "@angular/core/testing";
 import { StoreModule } from "@ngrx/store";
 import { HeaderComponent } from "./header.component";
+import { HeaderService } from "./header.service";
 
 describe('Component: Header', () => {
   beforeEach(() => {
@@ -18,5 +19,23 @@ describe('Component: Header', () => {
     let fixture = TestBed.createComponent(HeaderComponent);
     let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should use the header name from the header service', () => {
+    let fixture = TestBed.createComponent(HeaderComponent);
+    let app = fixture.debugElement.componentInstance;
+    let headerService = fixture.debugElement.injector.get(HeaderService);
+    fixture.detectChanges();
+    expect(headerService.title).toEqual(app.title);
+  });
+
+  it('should display the logout button', () => {
+    let fixture = TestBed.createComponent(HeaderComponent);
+    let app = fixture.debugElement.componentInstance;
+    let headerService = fixture.debugElement.injector.get(HeaderService);
+    app.isAuthenticated = true;
+    fixture.detectChanges();
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.logout-btn')).toBeTruthy();
   });
 });
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,22 +6,27 @@ import { User } from "../auth/user.model";
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from '../auth/store/auth.actions';
 import * as RecipeActions from '../recipes/store/recipe.actions';
+import { HeaderService } from "./header.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  providers: [HeaderService]
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   userSub = new Subscription();
   isAuthenticated = false;
   collapsed = true;
+  title: string;
 
   constructor(
-    private store: Store<fromApp.AppState>
+    private store: Store<fromApp.AppState>,
+    private headerService: HeaderService
   ) {}
 
   ngOnInit() {
+    this.title = this.headerService.title;
     this.userSub = this.store.select('auth').pipe(
       map(authState => authState.user)
     ).subscribe((user: User) => {
